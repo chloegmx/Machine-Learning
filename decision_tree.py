79925e5f4a582eb271c52f7d9dc91cc99f54d7c7
Ananya Priya <135134362+chloegmx@users.noreply.github.com>
2023-06-03 01:25:11 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-df = pd.read_csv("heart.csv")
+df = pd.read_csv("breast_cancer.csv")
 df.head()
 
 #pre-processing 
